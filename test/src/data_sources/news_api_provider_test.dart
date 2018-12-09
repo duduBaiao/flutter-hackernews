e0ca8f9f5c3f104ac30cdef9db255a08717fddd0
Eduardo Baiao <eduardobaiao@gmail.com>
2018-12-09 19:16:53 +0100

    Add test to the "fetch item" method.

--- test/src/data_sources/news_api_provider_test.dart
+++ test/src/data_sources/news_api_provider_test.dart
@@ -1,23 +1,36 @@
 import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:news/src/data_sources/news_api_provider.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+import 'package:news/src/data_sources/news_api_provider.dart';
 
 void main() {
   test('should fetch top ids', () async {
-    final newsApiProvider = NewsApiProvider();
-
     final mockClient = MockClient((request) async {
       final body = json.encode([1, 2, 3, 4]);
       return Response(body, 200);
     });
 
+    final newsApiProvider = NewsApiProvider();
     newsApiProvider.client = mockClient;
 
     final ids = await newsApiProvider.fetchTopIds();
 
     expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('should fetch item', () async {
+    final mockClient = MockClient((request) async {
+      final item = {'id': 123};
+      return Response(json.encode(item), 200);
+    });
+
+    final newsApiProvider = NewsApiProvider();
+    newsApiProvider.client = mockClient;
+
+    final item = await newsApiProvider.fetchItem(123);
+
+    expect(item.id, 123);
   });
 }
