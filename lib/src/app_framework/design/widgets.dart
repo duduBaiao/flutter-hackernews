424411ef9a7f7858832692164d1136e72aff7361
Eduardo Baiao <eduardobaiao@gmail.com>
2018-12-15 01:01:10 +0100

    Delete unused widgets.

--- lib/src/app_framework/design/widgets.dart
+++ lib/src/app_framework/design/widgets.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/app_framework/design/metrics.dart';
 
-Widget centeredMessage(String message) {
-  return Center(
-    child: Text(message),
-  );
-}
-
-Widget simpleTitle(String title) {
-  return Container(
-    height: Metrics.list.item.defaultHeight,
-    child: ListTile(
-      title: centeredMessage(title),
-    ),
-  );
-}
-
 Widget loadingTitle() {
   return Container(
     height: Metrics.list.item.defaultHeight,
