c24c95bc155192ba9b997c9c4e16f785457ab728
Eduardo Baiao <eduardobaiao@gmail.com>
2018-12-16 18:47:05 +0100

    Remove the page navigator from the dependency injection.

--- lib/src/app_framework/app.dart
+++ lib/src/app_framework/app.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_simple_dependency_injection/injector.dart';
 import 'package:news/src/app_framework/views/page_navigator.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final PageNavigator _pageNavigator = Injector.getInjector().get<PageNavigator>();
-
     return MaterialApp(
       title: 'News!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      onGenerateRoute: _pageNavigator.onGenerateRoute,
+      onGenerateRoute: PageNavigator().onGenerateRoute,
     );
   }
 }
