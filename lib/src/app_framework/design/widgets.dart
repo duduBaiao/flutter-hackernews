aafc7eddf2f124d3c5be97dab1b7038e1c5cef75
Eduardo Baiao <eduardobaiao@gmail.com>
2018-12-17 23:14:17 +0100

    Show the item title on the details page.

--- lib/src/app_framework/design/widgets.dart
+++ lib/src/app_framework/design/widgets.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/app_framework/design/metrics.dart';
 
+Widget padding(Widget child) {
+  return Padding(
+    padding: EdgeInsets.all(Metrics.layout.padding),
+    child: child,
+  );
+}
+
 Widget progressIndicator() {
   return Center(
     child: CircularProgressIndicator(),
--- lib/src/app_framework/views/item_detail/item_detail_page.dart
+++ lib/src/app_framework/views/item_detail/item_detail_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/app_framework/design/styles.dart';
+import 'package:news/src/app_framework/design/widgets.dart';
 import 'package:news/src/app_framework/views/item_detail/item_detail_scoped_model.dart';
+import 'package:news/src/domain/models/item_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class ItemDetailPage extends StatelessWidget {
@@ -9,14 +12,45 @@ class ItemDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = ScopedModel.of<ItemDetailScopedModel>(context).viewModel;
-    viewModel.fetchItemWithComments(itemId);
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Item Detail"),
       ),
-      body: Text("Item id: $itemId"),
+      body: _bodyBuilder(context),
+    );
+  }
+
+  Widget _bodyBuilder(BuildContext context) {
+    final viewModel = ScopedModel.of<ItemDetailScopedModel>(context).viewModel;
+    viewModel.fetchItemWithComments(itemId);
+
+    return StreamBuilder(
+      stream: viewModel.commentItems,
+      builder: (BuildContext context, AsyncSnapshot<Map<int, Future<ItemModel>>> commentItemsSnapshot) {
+        if (!commentItemsSnapshot.hasData) {
+          return progressIndicator();
+        }
+
+        return _titleFutureBuilder(commentItemsSnapshot);
+      },
+    );
+  }
+
+  FutureBuilder<ItemModel> _titleFutureBuilder(AsyncSnapshot<Map<int, Future<ItemModel>>> commentItemsSnapshot) {
+    final itemFuture = commentItemsSnapshot.data[itemId];
+
+    return FutureBuilder(
+      future: itemFuture,
+      builder: (BuildContext context, AsyncSnapshot<ItemModel> itemSnapshot) {
+        return padding(itemSnapshot.hasData ? _title(itemSnapshot.data) : Text('Loading item...'));
+      },
+    );
+  }
+
+  Widget _title(ItemModel item) {
+    return Text(
+      item.title,
+      style: Styles.text.title,
     );
   }
 }
