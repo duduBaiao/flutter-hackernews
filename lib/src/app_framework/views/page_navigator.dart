2418ca8e746230a58b424e036a0eb5230c42cbf5
Eduardo Baiao <eduardobaiao@gmail.com>
2018-12-18 21:37:04 +0100

    Improve page navigator class. Now route urls are not required.

--- lib/src/app_framework/views/page_navigator.dart
+++ lib/src/app_framework/views/page_navigator.dart
@@ -6,25 +6,18 @@ import 'package:news/src/app_framework/views/stories/stories_scoped_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class PageNavigator {
-  static const _ITEM_DETAIL_PAGE = "/item_detail/";
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return _pageRoute(child: _storiesPage());
+  }
 
   pushItemDetailPage(BuildContext context, int itemId) {
-    Navigator.pushNamed(context, "$_ITEM_DETAIL_PAGE$itemId");
+    Navigator.push(
+      context,
+      _pageRoute(child: _itemDetailPage(itemId)),
+    );
   }
 
-  Route<dynamic> onGenerateRoute(RouteSettings settings) {
-    var pageWidget;
-
-    if (settings.name.startsWith(_ITEM_DETAIL_PAGE)) {
-      int itemId = _extractIntParam(settings);
-      pageWidget = _itemDetailPageRoute(itemId);
-    } else {
-      pageWidget = _storiesPage();
-    }
-    return _pageRoute(pageWidget);
-  }
-
-  MaterialPageRoute _pageRoute(Widget child) {
+  MaterialPageRoute _pageRoute({Widget child}) {
     return MaterialPageRoute(builder: (BuildContext context) {
       return ScopedModel<PageNavigatorScopedModel>(
         model: PageNavigatorScopedModel(pageNavigator: this),
@@ -40,15 +33,11 @@ class PageNavigator {
     );
   }
 
-  Widget _itemDetailPageRoute(int itemId) {
+  Widget _itemDetailPage(int itemId) {
     return ScopedModel<ItemDetailScopedModel>(
       model: ItemDetailScopedModel(),
       child: ItemDetailPage(itemId: itemId),
     );
-  }
-
-  int _extractIntParam(RouteSettings settings) {
-    return int.parse(settings.name.split("/").last);
   }
 }
 
